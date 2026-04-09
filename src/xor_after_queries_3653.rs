4f591f84ba0183986b6d1028d844edb6a005d4d3
Vasili Gulevich <vasili.gulevich@xored.com>
2026-04-09 21:00:37 +0400

    Remove casting checks

--- src/xor_after_queries_3653.rs
+++ src/xor_after_queries_3653.rs
@@ -18,12 +18,42 @@ pub fn xor_after_queries(mut nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
     nums.into_iter().reduce(|a, b| a ^ b).unwrap_or(0)
 }
 
+pub fn xor_after_queries_unchecked(mut nums: Vec<i32>, queries: Vec<Vec<i32>>) -> i32 {
+    for query in queries {
+        let (l, r, k, v) = {
+            (
+                query[0] as usize,
+                query[1] as usize,
+                query[2] as usize,
+                i64::from(query[3]),
+            )
+        };
+        for idx in (l..=r).step_by(k) {
+            let num = &mut nums[idx];
+            let temp = (i64::from(*num) * v) % M;
+            *num = temp as i32;
+        }
+    }
+    nums.into_iter().reduce(|a, b| a ^ b).unwrap_or(0)
+}
+
 #[test]
 fn official() {
     assert_eq!(4, xor_after_queries(vec!(1, 1, 1), to_vector(&[[0, 2, 1, 4]])));
     assert_eq!(
+        4,
+        xor_after_queries_unchecked(vec!(1, 1, 1), to_vector(&[[0, 2, 1, 4]]))
+    );
+    assert_eq!(
         31,
         xor_after_queries(
+            vec!(2, 3, 1, 5, 4),
+            vec!(vec!(1, 4, 2, 3), vec!(0, 2, 1, 2))
+        )
+    );
+    assert_eq!(
+        31,
+        xor_after_queries_unchecked(
             vec!(2, 3, 1, 5, 4),
             vec!(vec!(1, 4, 2, 3), vec!(0, 2, 1, 2))
         )
@@ -34,6 +64,7 @@ fn official() {
 fn error1() {
     let input = [[0,0,1,13],[0,0,1,17],[0,0,1,9],[0,0,1,18],[0,0,1,16],[0,0,1,6],[0,0,1,4],[0,0,1,11],[0,0,1,7],[0,0,1,18],[0,0,1,8],[0,0,1,15],[0,0,1,12]];
     assert_eq!(523618060, xor_after_queries(vec!(780), to_vector(&input)));
+    assert_eq!(523618060, xor_after_queries_unchecked(vec!(780), to_vector(&input)));
 }
 
 #[cfg(test)]
