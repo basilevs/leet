68d02fc99e4d8cb4963dc6a9735bb460ee15dcdb
Vasili Gulevich <vasili.gulevich@xored.com>
2026-04-17 17:00:14 +0400

    Fix last official test

--- src/minimum_distance_mirror_3761.rs
+++ src/minimum_distance_mirror_3761.rs
@@ -4,12 +4,11 @@ use std::collections::HashMap;
 
 
     pub fn min_mirror_pair_distance(nums: Vec<i32>) -> i32 {
-        let mut last_position_by_value: HashMap<i32, usize> = HashMap::new();
+        let mut last_position_by_value: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
         nums.into_iter().enumerate().filter_map(|(position, value)| {
-            let value = remove_trailing_zeros(value);
             let mirror = mirror(value);
-            let prev_position_option = last_position_by_value.get(&mirror).copied();
-            last_position_by_value.insert(value, position);
+            let prev_position_option = last_position_by_value.get(&value).copied();
+            last_position_by_value.insert(mirror, position);
             // dbg!(&position, &value, &mirror, &prev_position_option);
             prev_position_option.map(|p| (position - p) as i32)
         }).min().unwrap_or(-1)
@@ -25,13 +24,6 @@ fn mirror(mut input:i32) -> i32 {
     result
 }
 
-#[inline]
-fn remove_trailing_zeros(mut input:i32) -> i32 {
-    while input > 0 && input % 10 == 0 {
-        input /= 10;
-    }
-    input
-}
 
 #[test]
 fn official1() {
@@ -41,5 +33,9 @@ fn official1() {
 #[test]
 fn official2() {
     assert_eq!(1, min_mirror_pair_distance([120,21].to_vec()));
-    assert_eq!(1, min_mirror_pair_distance([21,120].to_vec()));
+}
+
+#[test]
+fn official3() {
+    assert_eq!(-1, min_mirror_pair_distance([21,120].to_vec()));
 }
