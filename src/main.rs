0aa334dfe184a8bcfe0a2a59183427e9a289a64f
Vasili Gulevich <vasili.gulevich@xored.com>
2026-05-21 21:35:10 +0400

    Fix compilation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+use std::{collections::HashSet, iter::successors};
+
 fn main() {
-    let a = u32::MAX;
     let n = 12345;
-            let arr1_prefixes: HashSet<i32> = arr1
-            .iter()
-            .flat_map(|&x| successors(Some(x), |&n| (n >= 10).then_some(n / 10)))
-            .collect();
-    println!("Hello World! {} ", n);
+    let arr1_prefixes: HashSet<i32> = successors(Some(n), |&n| (n >= 10).then_some(n / 10))
+    .collect();
+    println!("Hello World! {:?} ", &arr1_prefixes);
 }
