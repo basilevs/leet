1b5852e57fd5401912ec6f3b20e493abaa6b0cd0
Vasili Gulevich <vasili.gulevich@xored.com>
2026-05-24 22:58:06 +0400

    A bit more tests and a bug

--- src/max_jumps_1340.rs
+++ src/max_jumps_1340.rs
@@ -11,13 +11,13 @@ pub fn max_jumps(arr: Vec<i32>, d: i32) -> i32 {
     indices_by_height_desc.sort_by_key(|&i| -arr[i]);
     for i in indices_by_height_desc {
         let current_depth = depth[i];
-        for j in i.saturating_add(1).min(n-1)..=i.saturating_add(d).min(n-1) {
+        for j in i.saturating_add(1)..=i.saturating_add(d).min(n-1) {
             if arr[j] >= arr[i] {
                 break;
             }
             depth[j] = depth[j].max(current_depth + 1);
         }
-        for j in (i.saturating_sub(d)..i.saturating_sub(1)).rev() {
+        for j in (i.saturating_sub(d)..i).rev() {
             if arr[j] >= arr[i] {
                 break;
             }
@@ -40,4 +40,19 @@ fn official2() {
 #[test]
 fn official3() {
     assert_eq!(7, max_jumps(vec![7,6,5,4,3,2,1], 1));
+}
+
+#[test]
+fn t1() {
+    assert_eq!(1, max_jumps(vec![7], 1));
+}
+
+#[test]
+fn t2() {
+    assert_eq!(2, max_jumps(vec![1,2], 1));
+}
+
+#[test]
+fn t3() {
+    assert_eq!(2, max_jumps(vec![2,1], 1));
 }
