9be38a4f794ce50f0c50eb20d0ba962237a23dd2
Vasili Gulevich <vasili.gulevich@xored.com>
2026-06-16 15:01:52 +0400

    AI api hists + manual allocation optimization

--- src/process_str_3612.rs
+++ src/process_str_3612.rs
@@ -1,14 +1,15 @@
 pub fn process_str(s: String) -> String {
-    let mut result:Vec<u8> = Vec::new();
-    let mut buffer:Vec<u8> = Vec::new();
+    let dups = s.bytes().filter(|x| *x == b'#').count();
+    let mut result: Vec<u8> = Vec::with_capacity(s.len() * dups);
     for b in s.into_bytes() {
         match b {
-            b'*' => {result.pop();},
+            b'*' => {
+                result.pop();
+            }
             b'#' => {
-                buffer.clear();
-                buffer.extend_from_slice(&result);
-                result.extend_from_slice(&buffer);
-            },
+                let len = result.len();
+                result.extend_from_within(..len);
+            }
             b'%' => result.reverse(),
             _ => result.push(b),
         }
