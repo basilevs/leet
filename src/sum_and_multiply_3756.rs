0f346837841f7be5cb7c5e2d47cb4e052a72be86
Vasili Gulevich <vasili.gulevich@xored.com>
2026-07-08 12:41:09 +0400

    sum_and_multiply 3756 AI review + optimization

--- src/sum_and_multiply_3756.rs
+++ src/sum_and_multiply_3756.rs
@@ -4,18 +4,33 @@ use crate::modint::ModInt;
 
 pub fn sum_and_multiply(s: String, queries: Vec<Vec<i32>>) -> Vec<i32> {
     let bs = s.into_bytes();
+    let total_len: usize = queries.iter().map(|q| (q[1] - q[0]) as usize).sum();
+    if total_len < bs.len() {
+        return queries.into_iter().map(|q| {
+            let l: usize = q[0].try_into().expect("0 <= l < m");
+            let r: usize = q[1].try_into().expect("0 <= r < m");
+            let (x, sum) = bs[l..=r].iter().fold((ModInt::from(0), 0u32), |(x, sum), b| {
+                if *b == b'0' {
+                    (x, sum)
+                } else {
+                    (x * ModInt::from(10) + ModInt::from(b - b'0'), sum + u32::from(b - b'0'))
+                }
+            });
+            x * ModInt::from(sum)
+        }).map(ModInt::into).collect();
+    }
     // (digits, sum, non_zero_counts)
-    let prefixes = bs.iter().scan((ModInt::from(0), 0, 0), |acc, b| {
-        if *b != b'0' {
+    let prefixes = bs.into_iter().scan((ModInt::from(0), 0u32, 0u32), |acc, b| {
+        if b != b'0' {
             acc.0 = acc.0 * ModInt::from(10) + ModInt::from(b - b'0');
-            acc.1 += b - b'0';
+            acc.1 += u32::from(b - b'0');
             acc.2 += 1;
         }
         Some(*acc)
     }).collect::<Vec<_>>();
     queries.into_iter().map(|q| {
-        let l: usize  = q[0].try_into().expect("0 <= l < m");
-        let r: usize  = q[1].try_into().expect("0 <= r < m");
+        let l: usize = q[0].try_into().expect("0 <= l < m");
+        let r: usize = q[1].try_into().expect("0 <= r < m");
         if l == 0 {
             prefixes[r].0 * ModInt::from(prefixes[r].1)
         } else {
@@ -79,5 +94,15 @@ mod tests {
         ];
         let expected = vec![4];
         assert_eq!(expected, sum_and_multiply(s, to_queries(&queries)));
+    }
+
+    #[test]
+    fn large_digit_sum() {
+        // 40 nines: x = 40 concatenated nines, sum = 40 * 9 = 360.
+        // The digit sum exceeds 255, exercising accumulation beyond a byte.
+        let s = "9".repeat(40);
+        // x mod (1e9+7) for "9" * 40, then multiplied by 360, taken mod (1e9+7).
+        let expected = vec![643_599_584];
+        assert_eq!(expected, sum_and_multiply(s, to_queries(&[[0, 39]])));
     }
 }
