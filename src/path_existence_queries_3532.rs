d7b4f4c1390d04057c53b569c69c0b7dfaff8cf2
Vasili Gulevich <vasili.gulevich@xored.com>
2026-07-09 13:58:44 +0400

    path_existence_queries 3532 avoid log lookup, peeking

--- src/path_existence_queries_3532.rs
+++ src/path_existence_queries_3532.rs
@@ -1,22 +1,28 @@
 // https://leetcode.com/problems/path-existence-queries-in-a-graph-i
 
-use std::collections::BTreeSet;
-
 pub fn path_existence_queries(
     _: i32,
-    nums: Vec<i32>,
+    mut nums: Vec<i32>,
     max_diff: i32,
     queries: Vec<Vec<i32>>,
 ) -> Vec<bool> {
     debug_assert!(nums.is_sorted());
-    let gaps: BTreeSet<i32> = nums.windows(2).filter(|w| w[1] - w[0] > max_diff).map(|w| w[0]).collect();
+    let mut component = 0;
+    let mut last_value = nums[0];
+    for x in nums.iter_mut() {
+        if *x - last_value > max_diff {
+            component += 1;
+        }
+        last_value = *x;
+        *x = component;
+    }
     queries.into_iter().map(|q| {
-        let (l, r) = (q[0], q[1]);
+        let l:usize = q[0].try_into().expect("q[0] should be a valid nums index");
+        let r:usize = q[1].try_into().expect("q[1] should be a valid nums index");
         if l == r {
             true
         } else {
-            let (l, r) = (nums[l as usize], nums[r as usize]);
-            gaps.range(l..r).next().is_none()
+            nums[l] == nums[r]
         }
     }).collect()
 }
@@ -51,6 +57,26 @@ mod tests {
     fn official2() {
         let n = 4;
         let nums = vec![2, 5, 6, 8];
+        let max_diff = 2;
+        #[rustfmt::skip]
+        let queries = [
+            [0, 1],
+            [0, 2],
+            [1, 3],
+            [2, 3],
+        ];
+
+        let expected = vec![false, false, true, true];
+        assert_eq!(
+            expected,
+            path_existence_queries(n, nums, max_diff, to_queries(&queries))
+        );
+    }
+
+        #[test]
+    fn mismatch_values_indices() {
+        let n = 4;
+        let nums = vec![12, 15, 16, 18];
         let max_diff = 2;
         #[rustfmt::skip]
         let queries = [
