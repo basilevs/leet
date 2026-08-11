c752369e19da3a2657fde8e87a4f38472d06f81a
Vasili Gulevich <vasili.gulevich@xored.com>
2026-08-11 21:58:42 +0400

    winner_square_game 1510 manual

--- rust/src/winner_square_game_1510.rs
+++ rust/src/winner_square_game_1510.rs
@@ -1,26 +1,59 @@
 // https://leetcode.com/problems/stone-game-iv
 
-pub fn winner_square_game(n: i32) -> bool {
-    dbg!(n);
-    todo!("training scaffold: implement solution");
+use std::{collections::HashMap, cell::RefCell};
+
+thread_local! {
+    static CACHE: RefCell<HashMap<i32, bool>> = RefCell::new(HashMap::new());
 }
 
+pub fn winner_square_game(n: i32) -> bool {
+    {
+        let result_option = CACHE.with(|cache| cache.borrow().get(&n).copied());
+        if let Some(r) = result_option {
+            return r;
+        }
+    }
+
+    let sqrt = (n as f64).sqrt() as i32;
+    let mut result = false;
+    if is_square(n) {
+        result = true;
+    } else {
+        for i in 1..=sqrt {
+            let square = i * i;
+            if !winner_square_game(n - square) {
+                result = true;
+                break;
+            }
+        }
+    }
+
+    CACHE.with(|c| c.borrow_mut().insert(n, result));
+    result
+}
+
+fn is_square(n: i32) -> bool {
+    n >= 0 && {
+        let r = n.isqrt();
+        r * r == n
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::winner_square_game;
 
     #[test]
     fn official1() {
-        assert_eq!(true, winner_square_game(1));
+        assert!(winner_square_game(1));
     }
 
     #[test]
     fn official2() {
-        assert_eq!(false, winner_square_game(2));
+        assert!(!winner_square_game(2));
     }
 
     #[test]
     fn official3() {
-        assert_eq!(true, winner_square_game(4));
+        assert!(winner_square_game(4));
     }
 }
